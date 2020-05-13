278fc559ddbab283cae9e6a5dd235b65eca3e0b5
Eric Manongdo <eric@superscriptcreative.com>
2020-05-12 16:10:02 -1000

    Public/Data 1.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/public/data/`,
+        path: `${__dirname}/public/data`,
       },
     },
     {
