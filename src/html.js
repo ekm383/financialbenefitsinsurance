0da439047ef703dd57bf5c1d5ea5f56e8faa6296
ekm383 <ekm383@gmail.com>
2020-05-14 21:28:25 -1000

    Zoho reset

--- src/html.js
+++ src/html.js
@@ -22,13 +22,6 @@ export default function HTML(props) {
         />
         {props.postBodyComponents}
 
-        <script type="text/javascript">
-          var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq ||
-{widgetcode:"cf0104f2ad79a1c9bd1317944221a1cca69fe57dd2d734f95e07923c16158c2e4fc7c4b2301c4343917922dfdbff702120d4f486795c2f2595dbaeaf7d5679df", values:{},ready:function(){}};
-var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
-s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
-</script>
-
       </body>
     </html>
   )
