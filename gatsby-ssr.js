63323101ad1c38902f3d83aaacf1298410129f91
ekm383 <ekm383@gmail.com>
2020-05-14 22:26:05 -1000

    Chat Added Zoho

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require("react")
 
-// You can delete this file if you're not using it
+const widgetcode = "cf0104f2ad79a1c9bd1317944221a1cca69fe57dd2d734f95e07923c16158c2e4fc7c4b2301c4343917922dfdbff702120d4f486795c2f2595dbaeaf7d5679df"; //Insert your salesiq widget code here.
+const src = "https://salesiq.zoho.com/widget"
+exports.onRenderBody = ({
+  setPostBodyComponents,
+}) => {
+  setPostBodyComponents([
+    <script type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `var $zoho = $zoho || {};
+                                                        var obj = {widgetcode:"${widgetcode}", values:{},ready:function(){}};
+                                                        $zoho.salesiq = $zoho.salesiq || obj
+                                                        var d = document;
+                                                        s = d.createElement("script");
+                                                        s.type = "text/javascript";
+                                                        s.id = "zsiqscript";
+                                                        s.defer = true;
+                                                        s.src = "${src}";
+                                                        t = d.getElementsByTagName("script")[0];
+                                                        t.parentNode.insertBefore(s, t);
+                                                        d.write("<div id='zsiqwidget'></div>");`}} /> //Injecting SalesIq Script on body of the page
+  ])
+}
